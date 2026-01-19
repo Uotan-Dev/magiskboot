c8b41093b2005ade787358a6dcdf97e49be63bf2
Killy Jack <2831906503@qq.com>
2026-01-19 22:59:13 +0800

    Update clone_submodules.sh

--- scripts/clone_submodules.sh
+++ scripts/clone_submodules.sh
@@ -3,8 +3,15 @@
 # fail fast
 set -e
 
+# Get number of cores
+if [ "$(uname)" = "Darwin" ]; then
+    NPROC=$(sysctl -n hw.ncpu)
+else
+    NPROC=$(nproc)
+fi
+
 set -x
-git submodule update --init --depth=1 --single-branch -j $(nproc) "$@" \
+git submodule update --init --depth=1 --single-branch -j $NPROC "$@" \
     src/{Magisk,android_{core,fmtlib,libbase,logging},corrosion,mman-win32,msvc_getline}/
-git -C src/Magisk submodule update --init --depth=1 --single-branch -j $(nproc) "$@" native/src/external/cxx-rs/
+git -C src/Magisk submodule update --init --depth=1 --single-branch -j $NPROC "$@" native/src/external/cxx-rs/
 set +x
